f128d162386e5337dbbdd539f1b63a86f3900a4f
Jakob Hansen <jakobhansen@microsoft.com>
2025-03-16 22:27:28 +0100

    Render framebuffer

---
+++ src/context/chip8_context.rs
@@ -0,0 +1,86 @@
+use sdl2::{pixels::Color, rect::Rect, render::Canvas, video::Window};
+
+pub const WIDTH: usize = 64;
+pub const HEIGHT: usize = 32;
+pub const SCALE: u32 = 10;
+
+#[derive(Debug)]
+pub struct Chip8Context {
+    // RAM
+    memory: [u8; 4096],
+
+    //  Registers
+    v: [u8; 16],
+
+    stack: [u16; 16],
+    // Special registers
+
+    // Framebuffer
+    pub frame_buffer: FrameBuffer,
+}
+
+impl Chip8Context {
+    pub fn new() -> Self {
+        Chip8Context {
+            memory: [0; 4096],
+            v: [0; 16],
+            stack: [0; 16],
+            frame_buffer: FrameBuffer::new(),
+        }
+    }
+}
+
+impl Default for Chip8Context {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[derive(Debug)]
+pub struct FrameBuffer {
+    buffer: [bool; WIDTH * HEIGHT],
+}
+
+impl FrameBuffer {
+    pub fn new() -> Self {
+        FrameBuffer {
+            buffer: [false; WIDTH * HEIGHT],
+        }
+    }
+    pub fn get_pixel(&self, x: usize, y: usize) -> Option<bool> {
+        let index = x * HEIGHT + y;
+        self.buffer.get(index).copied()
+    }
+    pub fn set_pixel(&mut self, x: usize, y: usize, value: bool) {
+        let index = x * HEIGHT + y;
+        if let Some(elem) = self.buffer.get_mut(index) {
+            *elem = value;
+        }
+    }
+    pub fn render(&self, canvas: &mut Canvas<Window>) {
+        canvas.set_draw_color(Color::BLACK);
+        canvas.clear();
+        canvas.set_draw_color(Color::WHITE);
+
+        for y in 0..HEIGHT {
+            for x in 0..WIDTH {
+                if self.get_pixel(x, y).expect("Invalid index") {
+                    let rect = Rect::new(
+                        (x as u32 * SCALE) as i32,
+                        (y as u32 * SCALE) as i32,
+                        SCALE,
+                        SCALE,
+                    );
+                    let _ = canvas.fill_rect(rect);
+                }
+            }
+        }
+        canvas.present();
+    }
+}
+
+impl Default for FrameBuffer {
+    fn default() -> Self {
+        Self::new()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+use chip8_rs::context::chip8_context::{Chip8Context, HEIGHT, SCALE, WIDTH};
+use sdl2::{event::Event, keyboard::Keycode};
+
+fn main() -> Result<(), String> {
+    let mut chip8 = Chip8Context::new();
+
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let window = video_subsystem
+        .window(
+            "CHIP-8 Emulator",
+            WIDTH as u32 * SCALE,
+            HEIGHT as u32 * SCALE,
+        )
+        .position_centered()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    chip8.frame_buffer.set_pixel(1, 1, true);
+    chip8.frame_buffer.set_pixel(1, 3, true);
+    chip8.frame_buffer.set_pixel(1, 4, true);
+    chip8.frame_buffer.set_pixel(2, 4, true);
+    chip8.frame_buffer.set_pixel(3, 4, true);
+    chip8.frame_buffer.set_pixel(4, 4, true);
+    chip8.frame_buffer.set_pixel(4, 3, true);
+    chip8.frame_buffer.set_pixel(4, 1, true);
+
+    let mut canvas = window
+        .into_canvas()
+        .present_vsync()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut event_pump = sdl_context.event_pump()?;
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        chip8.frame_buffer.render(&mut canvas);
+    }
+
+    Ok(())
 }
